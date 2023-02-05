db78ce310691782ebb8b3983232610c94fce64a6
NiklasHohmann <67792281+NiklasHohmann@users.noreply.github.com>
2023-02-05 11:37:51 +0100

    Expanded workflow

--- astrochron_pipeline.R
+++ astrochron_pipeline.R
@@ -1,10 +1,11 @@
-#### Load packages ####
-
-if (!require(stringr)) install.packages("stringr")
-require(stringr)
-
 #### load auxiliary functions ####
 source("aux_functions.R")
+
+#### Load packages ####
+# add required packages to this vector to automatically load them (and install if necessary)
+required_packages=c("stringr","astrochron")
+
+loadPackages(required_packages)
 
 #### read data ####
 sim_output=readFortran(dir=getwd(),filename_data="amarlx",filename_params="params")
@@ -20,6 +21,28 @@ depth_data=time_to_depth(time_data)
 time_data$time_years=time_data$time_dimless*Tstar
 depth_data$depth_cm=depth_data$depth_dimless*Xstar
 
-
+#### visualize output Calcite ####
 plot(depth_data$depth_cm/100,depth_data$calcite_prop,type="l",xlab="depth (cm)")
 plot(time_data$time_years,time_data$calcite_prop,type="l",xlab="time(years)")
+
+#### trim strat data ####
+burn_in_cm=8000
+sampling_dist_cm=10
+
+
+
+if ((burn_in_cm+sampling_dist_cm) > max(depth_data$depth_cm)) stop("burn in longer than sed column")
+depth_new=seq(from = burn_in_cm,
+               to = max(depth_data$depth_cm),
+               by = sampling_dist_cm)
+
+depth_data_interp=data.frame(depth_cm = depth_new)
+
+for (colname in colnames(depth_data)){
+  if (colname %in% c("depth_dimless","depth_cm","velocity_solid_phase_dimless")){
+    next
+  }
+  depth_data_interp[,colname]=approx(x=depth_data$depth_cm,y=depth_data[,colname],xout = depth_new)[[2]]
+  
+}
+plot(depth_data_interp$depth_cm/100,depth_data_interp$calcite_prop,type="l")
--- aux_functions.R
+++ aux_functions.R
@@ -1,47 +1,77 @@
+#### Auxiliary functions needed for the astrochron pipeline
 
-## Import data from FOrtran
+#### import all required packages ####
+loadPackages=function(packagenames){
+  for (package_name in required_packages){
+    if (!require(package_name,character.only = TRUE)) install.packages(package_name)
+    require(package_name,character.only = TRUE)
+  }
+}
+
+#### Import data from Fortran ####
+
 readFortran = function(dir=getwd(),filename_data="amarlx",filename_params="params"){
+# looks in the directory dir for output files written by Fortran, and reads 
+# the values at the bottom of the system into R
+# currently (5th Feb 2023) only works with the "corrected by lheureux" branch
   data=read.table(paste(dir,"/",filename_data,sep=""),header=TRUE)
-  data2=data[,c("t","CA.3","AR.3","P.3","U")]
   
+  data2=data[,c("t","CA.3","AR.3","P.3","U")] # data at bottom of system
+  
+  # convert vals to numeric
   for (colname in colnames(data2)){
     a=strsplit(data2[,colname],split="D")
     data2[,colname]=sapply(a, function(x) as.numeric(x[1])*10^as.numeric(x[2]))
   }
-  
+  # adjust col names of data frame
   colnames(data2)=c("time_dimless","calcite_prop","aragonite_prop","porosity","velocity_solid_phase_dimless")
   
+  # read model parameters
   params=read.table(paste(dir,"/",filename_params,sep=""),header=FALSE)
   Xstar=params$V1
   Tstar=params$V2
+  
   return(list(df=data2,
               Xstar=Xstar,
               Tstar=Tstar))
 }
 
-## time to depth transformation ##
+#### Transfrom data from time to depth ####
+# uses velocity of solid phase to transform time series into depth series
 time_to_depth = function(time_data){
+  # warn user of negative velocities
   if (any(time_data$velocity_solid_phase_dimless <= 0)){
     warning("Negative velocities of solid phase")
-    reorder_depths=TRUE
+    reorder_depths=TRUE # if velocities are negative, depths need to be reordered
   }
+  # mean velocity within a time bin
   U_mean=0.5*(head(time_data$velocity_solid_phase_dimless,-1)+tail(time_data$velocity_solid_phase_dimless,-1))
+  # thickness of sediment accumulated over one time step
   depth_increment_dimless=U_mean*diff(time_data$time_dimless)
+  # total accumulated depth
   depth_dimless=cumsum(c(0,depth_increment_dimless))
+  # stop if a depth values appears twice -> potentially ambiguous values
   if (anyDuplicated(depth_dimless)){
     stop("Ambiguous result in depth domain")
   }
+  # store depth data in data frame
   depth_data=time_data
   depth_data$time_dimless=depth_dimless
   colnames(depth_data)[colnames(depth_data)=="time_dimless"]="depth_dimless"
+  # if velocities are negative, order depths to be strictly increasing
+  # and permutate all other values accordingly
   if(reorder_depths){
+    # vector of ordering indices
     new_order=sort(x=depth_data$depth_dimless,decreasing = FALSE,index.return=TRUE)$ix
+    # order values 
     for (colname in colnames(depth_data)){
       depth_data[,colname]=depth_data[,colname][new_order]
     }
   }
   return(depth_data)
 }
+
+
 
 ## Calculate U from Phi and model parameters
 # rho_s_0=2.8
